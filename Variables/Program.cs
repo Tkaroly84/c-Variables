a08eff4c2a3f9a9ba3091eb0b29167c2c74543b0
Tóth Károly <toth.karolycar84@gmail.com>
2024-06-13 22:15:00 +0200

    Add project files.

---
+++ Variables/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            i = 42;
+
+            int j = 74;
+
+            string aaa = "aaa";
+
+            string number = "100";
+            int x = int.Parse(number);
+
+            //Console.WriteLine(x);
+
+
+            int charnum = Console.Read();
+            Console.WriteLine(charnum);
+            Console.ReadLine();
+
+            string inputText = Console.ReadLine();
+            Console.WriteLine(inputText);
+
+            Console.ReadLine();
+
+
+            ConsoleKeyInfo key = Console.ReadKey();
+            Console.WriteLine(key.Key);
+
+
+        }
+    }
+}
